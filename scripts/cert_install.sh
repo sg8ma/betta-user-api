6ce667832808fa2b732bf647c50332c3362b6599
daichi sugiyama <daichi.sugiyama.public@gmail.com>
2024-02-13 21:32:15 +0900

    chore: server setting

--- scripts/cert_install.sh
+++ scripts/cert_install.sh
@@ -2,10 +2,10 @@
 DOMAIN='api.betta.fortuna.jp'
 CURRENT_YEAR=`date +%Y`
 CURRENT_MONTH=`date +%m`
-mkdir -p /var/www/html/${DOMAIN}/cert/${CURRENT_YEAR}/${CURRENT_MONTH}
+mkdir -p /var/www/html/${DOMAIN}/certs/${CURRENT_YEAR}/${CURRENT_MONTH}
 /home/ec2-user/.acme.sh/acme.sh --install-cert \
     -d ${DOMAIN} \
-    --key-file /var/www/html/${DOMAIN}/cert/${CURRENT_YEAR}/${CURRENT_MONTH}/privkey.pem \
-    --fullchain-file /var/www/html/${DOMAIN}/cert/${CURRENT_YEAR}/${CURRENT_MONTH}/fullchain.pem
-sed -i".bak" -e "s|/cert/[0-9]\{4\}/[0-9]\{2\}|/cert/${CURRENT_YEAR}/${CURRENT_MONTH}|g" /var/www/html/${DOMAIN}/etc/nginx.conf
+    --key-file /var/www/html/${DOMAIN}/certs/${CURRENT_YEAR}/${CURRENT_MONTH}/privkey.pem \
+    --fullchain-file /var/www/html/${DOMAIN}/certs/${CURRENT_YEAR}/${CURRENT_MONTH}/fullchain.pem
+sed -i".bak" -e "s|/certs/[0-9]\{4\}/[0-9]\{2\}|/certs/${CURRENT_YEAR}/${CURRENT_MONTH}|g" /var/www/html/${DOMAIN}/etc/nginx.conf
 ~                                                                                                                                    
