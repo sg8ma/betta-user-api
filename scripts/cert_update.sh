e4b0eb07ba2077f0ba65daa51d925cdb97e7181d
daichi sugiyama <daichi.sugiyama.public@gmail.com>
2024-02-13 21:51:46 +0900

    chore: server setting

--- scripts/cert_update.sh
+++ scripts/cert_update.sh
@@ -2,13 +2,14 @@
 DOMAIN='api.betta.fortuna.jp'
 CURRENT_YEAR=`date +%Y`
 CURRENT_MONTH=`date +%m`
+CURRENT_DAY=`date +%d`
 cd /var/www/html/${DOMAIN}/scripts
 . ./cert_install.sh
 cd /var/www/html/${DOMAIN}
 git reset HEAD
 git add cert/${CURRENT_YEAR}/${CURRENT_MONTH}
 git add etc/nginx.conf
-git commit -m "chore: update certficate ${CURRENT_YEAR}/${CURRENT_MONTH}"
+git commit -m "chore: certficate was auto updated at ${CURRENT_YEAR}.${CURRENT_MONTH}.${CURRENT_DAY}"
 git push
 #sudo chown root /var/www/html/${DOMAIN}/scripts/cert_update.sh
 #sudo chmod 755 /var/www/html/${DOMAIN}/scripts/cert_update.sh
